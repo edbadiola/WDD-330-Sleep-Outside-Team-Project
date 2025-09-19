06943e015e2efa25689dbe52e9ebefd1992235cb
edbadiola <u000681426@byui.edu>
2025-09-19 23:39:09 +0800

    Task: Product List sort

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -10,7 +10,7 @@ function productCardTemplate(product) {
         <p class="product-card__price">$${product.FinalPrice}</p>
       </a>
     </li>
-    `;
+  `;
 }
 
 export default class ProductList {
@@ -18,21 +18,42 @@ export default class ProductList {
     this.category = category;
     this.dataSource = dataSource;
     this.listElement = listElement;
+    this.originalList = []; // keep the data for re-sorting
   }
 
   async init() {
     const list = await this.dataSource.getData(this.category);
+    this.originalList = list;                // save
     this.renderList(list);
     document.querySelector(".title").textContent = this.category;
+
+    document.querySelector("#sort").addEventListener("change", (e) => {
+      this.sortAndRender(e.target.value);
+    });
   }
 
-  renderList(list) {
-    // const htmlStrings = list.map(productCardTemplate);
-    // this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
+  sortAndRender(option) {
+    let sorted = [...this.originalList];
 
-    // apply use new utility function instead of the commented code above
-    renderListWithTemplate(productCardTemplate, this.listElement, list);
+    switch (option) {
+      case "name-asc":
+        sorted.sort((a, b) => a.Name.localeCompare(b.Name));
+        break;
+      case "name-desc":
+        sorted.sort((a, b) => b.Name.localeCompare(a.Name));
+        break;
+      case "price-asc":
+        sorted.sort((a, b) => a.FinalPrice - b.FinalPrice);
+        break;
+      case "price-desc":
+        sorted.sort((a, b) => b.FinalPrice - a.FinalPrice);
+        break;
+    }
 
+    this.renderList(sorted, true); 
   }
 
+  renderList(list, clear = false) {
+    renderListWithTemplate(productCardTemplate, this.listElement, list, "afterbegin", clear);
+  }
 }
